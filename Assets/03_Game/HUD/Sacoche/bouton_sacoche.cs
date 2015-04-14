91d2f7bba15ff96d8e3c07460b95b0488448c3c6
Baptiste <b.billet@rubika-edu.com>
2015-04-14 20:19:01 +0200

    debug jerome

---
+++ Assets/03_Game/HUD/Sacoche/bouton_sacoche.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouton_sacoche : MonoBehaviour {
+
+	public GameObject bouton_xboxY;
+	public GameObject bouton_clavier;
+
+
+	// Update is called once per frame
+	void Update () {
+	
+		if(ControllerManager.instance.m_XboxMode==true)
+		{
+			bouton_xboxY.SetActive(true);
+			bouton_clavier.SetActive(false);
+		}
+		else
+		{
+			bouton_clavier.SetActive(true);
+			bouton_xboxY.SetActive(false);
+		}
+
+
+	}
+}
